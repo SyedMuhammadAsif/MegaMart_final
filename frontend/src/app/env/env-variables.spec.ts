d73a04f99ce6dd87cfeee8734e6416bac426d590
Asif <2430012@cognizant.com>
2025-10-10 12:02:18 +0530

    Remove env spec file

--- frontend/src/app/env/env-variables.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EnvVariables } from './env-variables';
-
-describe('EnvVariables', () => {
-  it('should create an instance', () => {
-    expect(new EnvVariables()).toBeTruthy();
-  });
-});
